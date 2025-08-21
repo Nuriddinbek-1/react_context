3355f60cce40cd68de4e28cc8d92d6f575715846
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2025-08-21 17:17:17 +0500

    if statement changed to && sign

--- src/components/Component3.jsx
+++ src/components/Component3.jsx
@@ -3,12 +3,7 @@ function Component3({ count, setCount }) {
     <div className="component3">
       <h2>Component3</h2>
       <h2>Counter: {count}</h2>
-      <button
-        onClick={() => {
-          if (count > 0) setCount(count - 1);
-        }}>
-        -
-      </button>
+      <button onClick={() => count > 0 && setCount(count - 1)}>-</button>
       <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
